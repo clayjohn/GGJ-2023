d49b9db2c4b59e9859c9df739e03c0fe1bc4322b
CalebJohn <git@calebjohn.ca>
2023-02-04 17:56:48 -0800

    Add Functional Main menu and transition to first level
    Level 1 is updated to the new size

--- Main.gd
+++ Main.gd
@@ -3,6 +3,18 @@ extends Control
 # Responsible for managing the game
 # Handles loading and unloading of scenes
 
-func _ready():
-	pass
+var levels = [preload("res://Level/Level1.tscn")]
 
+func _ready():
+	$Player.position.x = 1344/3/2
+	$Player.position.y = 768/3/2
+	
+func switch_level(level: int):
+	print("Switching to level ", level)
+	var current_level = $Level
+	remove_child.call_deferred(current_level)
+	current_level.queue_free.call_deferred()
+	add_child.call_deferred(levels[level-1].instantiate())
+	$Player.position.x = 30
+	$Player.position.y = 768/3/2
+	
---
+++ Menu/Menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_entered(body):
+	print("Enter the dungeon") # Replace with function body.
+	get_parent().switch_level(1)
