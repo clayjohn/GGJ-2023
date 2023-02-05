5be49a3764b42ae3dad38a1e4215705af11f6b33
CalebJohn <git@calebjohn.ca>
2023-02-05 13:15:40 -0800

    Add enemy (worm) sprites and animations
    fill level1 with worms

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -10,25 +10,25 @@ var last_epoch = 0
 @export var patrol_pattern: WALK = WALK.STRAFE
 @export var patrol_period: float = 3000#ms
 @export var strafe_dir = STRAFE_DIR.VERT
-@export var speed = 60
+@export var speed = 45
 
 func rand_unit():
 	return Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player = get_node("%Player")
+	player = get_parent().get_parent().get_player()
 	last_epoch = Time.get_ticks_msec()
 	randomize()
+	$AnimatedSprite2D.play("Idle")
 
 func follow_player(dt):
 	direction = (player.transform.origin - transform.origin).normalized()
-	
+
 func walk_strafe(dt):
 	if Time.get_ticks_msec() - last_epoch > patrol_period:
 		if strafe_dir == STRAFE_DIR.VERT:
 			if direction.x > 0.01 or direction.x < -0.01:
-				print(direction)
 				direction = Vector2(0, -1)
 			direction.y = -direction.y
 		else:
@@ -61,5 +61,21 @@ func _on_vision_body_entered(body):
 		can_see_player = true
 
 func _on_vision_body_exited(body):
+	pass
+
+
+func _on_attack_range_body_entered(body):
+	if body == player:
+		$AnimatedSprite2D.animation = "Attack"
+
+func _on_attack_range_body_exited(body):
+	if body == player:
+		$AnimatedSprite2D.animation = "Idle"
+
+
+func _on_long_site_body_entered(body):
+	pass
+
+func _on_long_site_body_exited(body):
 	if body == player:
 		can_see_player = false
